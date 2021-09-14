f4581de898e12894d8c68a3566e4ffebf89c9e51
unknown <ek.mazurkevich@gmail.com>
2021-09-14 21:23:00 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+		boolean isRainy = false;
+		boolean iAmTired = false;
+		boolean isEvening = false;
+		
+		if (isRainy && iAmTired && isEvening == false) {
+		System.out.println("Stay home");
+		} else {
+			System.out.println("Go for a walk");	
+		}	
     }
 }
