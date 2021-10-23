d2c611da6ca32b11d33e45eb6cd2ae8ad5f00a42
unknown <ek.mazurkevich@gmail.com>
2021-10-23 17:20:49 +0300

    OOP

--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
+import java.util.Scanner;
 public class Main {
-	public static int [][] MULTIPLICATION_TABLE;
-    public static void main(String[] args) {
-		System.out.println("Tablica Umnojeniya");
-		MULTIPLICATION_TABLE = new int [10][10];
-		for (int i = 1; i <= 10; i++){
-			for (int j = 1; j <= 10; j++){
-				MULTIPLICATION_TABLE[i-1][j-1] = i * j;
-				System.out.print(MULTIPLICATION_TABLE[i-1][j-1]);
-				System.out.print(" ");
+	public static void main (String[] args) {
+		Scanner scanner  = new Scanner (System.in);
+		double sum=0;
+		int count = 0;
+		while (true) {
+			number= scanner.nextDouble();
+			if (number ==0) {
+				break;
 			}
-			System.out.println();
+			count++;
+			sum+=number;
 		}
-    }
+        System.out.printeln("Average: " +sum / count);
+    }		
 }
 																																																																																																																															
