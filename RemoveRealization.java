2ad087b99c4af9016363af9999560adcb2c7d0ac
Katerina8mazur <90004047+Katerina8mazur@users.noreply.github.com>
2021-11-03 15:34:02 +0300

    Add files via upload

---
+++ RemoveRealization.java
@@ -0,0 +1,39 @@
+package OOP;
+
+public class RemoveRealization {
+    //package ru.kfu.itis.example3.impl;
+
+//import ru.kfu.itis.example3.base.MyList;
+
+    public class MyListImpl implements MyList {
+        private final String[] elements = new String[1000];
+        private int size = 0;
+
+        @Override
+        public void add(String element) {
+            elements[size] = element;
+            size++;
+        }
+
+        // TODO: Реализовать
+        @Override
+        public void remove(int index) {
+            size+=1;
+            elements[size]= "";
+            for (int i = index; i <= size - 1 ; i++) {
+                elements[i] = elements[i+1];
+            }
+            elements[size] = null;
+        }
+
+        @Override
+        public String get(int index) {
+            return elements[index];
+        }
+
+        @Override
+        public int size() {
+            return size;
+        }
+    }
+}
