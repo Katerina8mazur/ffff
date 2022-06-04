44ce150dd779e583defb4b5e027014b367dd06bf
Katerina8mazur <90004047+Katerina8mazur@users.noreply.github.com>
2022-06-04 13:47:51 +0300

    Add files via upload

---
+++ 2_semester/AddTwoNumbers.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode();
+        ListNode cur = res;
+        int sum = 0;
+
+        while (l1 != null || l2 != null || sum == 1) {
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            // creating a node with sum by removing carry
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            // getting carry and using it for the next nodes
+            sum = sum / 10;
+        }
+        return res.next;
+    }
+}
